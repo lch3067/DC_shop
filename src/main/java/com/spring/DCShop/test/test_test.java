3464b2039ce864f9907d2c53435c023b22bb21b9
kmc7320-dotcom <kmc7320@gmail.com>
2025-08-14 12:26:34 +0900

    생성1% 완료

--- src/main/java/com/spring/DCShop/test/test_test.java
+++ src/main/java/com/spring/DCShop/test/test_test.java
@@ -2,4 +2,21 @@ package com.spring.DCShop.test;
 
 public class test_test {
 	private String test;
+
+	public test_test() {
+		super();
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+	
+	
+	
 }
