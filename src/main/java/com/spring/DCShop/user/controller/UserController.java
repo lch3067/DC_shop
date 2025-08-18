75cd6fe0614432bebc6347ea294d85a7fea7194c
chan <chopoke@naver.com>
2025-08-18 16:58:59 +0900

    메인페이지추가

--- src/main/java/com/spring/DCShop/user/controller/UserController.java
+++ src/main/java/com/spring/DCShop/user/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.spring.DCShop.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping("main")
+	public String main() {
+		logger.info("=== url -> main ===");
+		
+		return "main";
+	}
 }
