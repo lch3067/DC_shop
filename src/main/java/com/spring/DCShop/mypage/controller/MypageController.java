ffd1b534fd395b25508821af9a941affb78208d6
lch3067 <lch3067@naver.com>
2025-09-01 11:30:28 +0900

    [test] - 마이페이지 테스트

--- src/main/java/com/spring/DCShop/mypage/controller/MypageController.java
+++ src/main/java/com/spring/DCShop/mypage/controller/MypageController.java
@@ -1,9 +1,15 @@
 package com.spring.DCShop.mypage.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.spring.DCShop.mypage.service.MypageService;
 
 
 
@@ -11,11 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MypageController {
 	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
 		
-	//@Autowired
+	@Autowired
+	private MypageService myService;
 	
 	@RequestMapping("mypage_main.do")
-	public String mypage_main() {
+	public String mypage_main(HttpServletRequest request, HttpServletResponse response, Model model) {
 		logger.info("=== url -> mypage_main ===");
+		
+		// 주문 상품 리스트 가져와서 뿌려주기
+		myService.getCartList(request, response, model);;
 		
 		return "/mypage/mypage_main";
 	}
--- src/main/java/com/spring/DCShop/mypage/dao/MypageDAO.java
+++ src/main/java/com/spring/DCShop/mypage/dao/MypageDAO.java
@@ -1,6 +1,14 @@
 package com.spring.DCShop.mypage.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.spring.DCShop.mypage.dto.CartDTO;
+import com.spring.DCShop.mypage.dto.OrderDTO;
+import com.spring.DCShop.mypage.dto.ProductDTO;
 
 public interface MypageDAO {
+	
+	public List<OrderDTO> getOrderList(Map<String, Object> productListInfo);
 	
 }
