100fcdb562cb619f2471cedb365835af23ed0b27
lch3067 <lch3067@naver.com>
2025-09-03 17:14:10 +0900

    [feat] - 마이페이지 주문목록리스트 기능 추가

--- src/main/java/com/spring/DCShop/mypage/dao/MypageDAOImpl.java
+++ src/main/java/com/spring/DCShop/mypage/dao/MypageDAOImpl.java
@@ -1,9 +1,32 @@
 package com.spring.DCShop.mypage.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.spring.DCShop.mypage.dto.OrderDTO;
 
 
 @Repository
 public class MypageDAOImpl implements MypageDAO{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<OrderDTO> getOrderList(Map<String, Object> productListInfo) {
+		
+		System.out.println("MypageDAOImpl => getCartList");
+		
+		List<OrderDTO> list = sqlSession.selectList("com.spring.DCShop.mypage.dao.MypageDAO.getOrderList", productListInfo);
+		
+		System.out.println(list);
+		
+		return list;
+		
+	}
 	
 }
